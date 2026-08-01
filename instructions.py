4a9209a41325b7418f1b0e5d8e6b4997ed348a78
RhemJos <rhemjos@gmail.com>
2026-08-01 11:40:09 -0400

    last

---
+++ instructions.py
@@ -0,0 +1,23 @@
+txt_instruction = '''
+Esta app nos permite \nrealizar un diagnóstico inicial de tu salud \n usando un test de Ruffier.
+Un test de Ruffier es una prueba de estrés\n realizada para evaluar la eficiencia del corazón durante esfuerzo físico.\n
+El usuario calcula su frecuencia de pulso por 15 segundos.\n
+Luego el usuario hace 30 sentadillas durante 45 segundos.\n
+Después del final del esfuerzo, el pulso es calculado de nuevo: \n el número de pulsaciones después de los primeros 15 segundos, 30 segundos de descanso \n y el número de pulsaciones en los últimos 15 segundos.\n'''
+
+
+txt_test1 = '''Mide tu pulso por 15 segundos.\n
+Escribe tus resultados en los campos correspondientes.'''
+
+
+txt_test2 = '''Realiza 30 sentadillas en 45 segundos.\n
+Presiona el botón “Iniciar” para ejecutar el contador de sentadillas.
+Haz las sentadillas a la velocidad del contador.'''
+
+
+txt_test3 = '''Mide tu pulso dos veces en el próximo minuto:\n
+durante los primeros 15 segundos y luego durante los últimos 15 segundos del minuto.\n
+Escribe tus resultados en los campos correspondientes.'''
+
+
+txt_sits = 'Realiza 30 sentadillas en 45 segundos.'
